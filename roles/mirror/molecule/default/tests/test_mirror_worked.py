d99e942cecfbf17cc8ddcc980097e8a67b0fab68
Gregory Hellings <greg.hellings@gmail.com>
2020-02-25 00:39:14 -0600

    Update mirror

---
+++ roles/mirror/molecule/default/tests/test_mirror_worked.py
@@ -0,0 +1,12 @@
+def test_mirror_script_exists(host):
+    assert host.file('/var/mirror/mirror.sh').exists
+
+
+def test_mirror_dir_exists(host):
+    f = host.file('/var/mirror/8/cr')
+    assert f.exists
+    assert f.is_directory
+
+
+def test_excludes_worked(host):
+    assert not host.file('/var/mirror/8/BaseOS').exists
