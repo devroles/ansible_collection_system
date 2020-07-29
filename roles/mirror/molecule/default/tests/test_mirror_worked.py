af246b86860fd384fa295a53381bd0bfeae6f521
Greg Hellings <greg.hellings@gmail.com>
2020-07-29 00:54:36 -0400

    Cleanup mirror role
    Sample source sets local dir to 775, so fixed idempotence test
    Introduced variable to put sync scripts outside target directory, to
    avoid them being overwritten or deleted by the rsync run

--- roles/mirror/molecule/default/tests/test_mirror_worked.py
+++ roles/mirror/molecule/default/tests/test_mirror_worked.py
@@ -1,9 +1,9 @@
 def test_mirror_script_exists(host):
-    assert host.file('/var/mirror/centos.sh').exists
+    assert host.file('/root/centos.sh').exists
 
 
 def test_mirror_dir_exists(host):
-    f = host.file('/var/mirror/8/cr')
+    f = host.file('/var/mirror/build')
     assert f.exists
     assert f.is_directory
 
