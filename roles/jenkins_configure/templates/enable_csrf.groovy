592cdc6b5741a480b1caf7e68279616705fc01ef
Gregory Hellings <greg.hellings@gmail.com>
2021-04-19 01:02:34 -0500

    I don't use or maintain Jenkins anymore
    Remove the Jenkins related roles. They're dead to me

--- roles/jenkins_configure/templates/enable_csrf.groovy
+++
@@ -1,9 +0,0 @@
-import hudson.security.csrf.DefaultCrumbIssuer;
-import jenkins.model.Jenkins;
-
-def jenkins = Jenkins.instance;
-if( jenkins.getCrumbIssuer() == null ) {
-	jenkins.setCrumbIssuer(new DefaultCrumbIssuer(true));
-	jenkins.save();
-	println "CHANGED: Created new issuer"
-}
--- roles/jenkins_master/files/mass_disable.groovy
+++
@@ -1,5 +0,0 @@
-for(item in hudson.model.Hudson.instance.items) {
-  println("Disabling " + item.name)
-  item.disabled = true
-  item.save()
-}
